2b9aae6eadf97cc88b297fbcb21a8b7c65439598
Simon Brecht <71435364+0x7c2@users.noreply.github.com>
2021-10-14 13:53:23 +0200

    Create CPbackupCLISH.sh

---
+++ CPbackupCLISH.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#
+# Copyright 2021 by by 0x7c2, Simon Brecht.
+# All rights reserved.
+# This file is used to delete specific connections from kernel tables,
+# and is released under the "Apache License 2.0". Please see the LICENSE
+# file that should have been included as part of this package.
+#
+
+FTP_HOST=ftp-server.domain.local
+FTP_USER=checkpoint-backup-user
+FTP_PASS=password
+
+TIMESTAMP=`date "+%Y.%m.%d-%H.%M.%S"`
+MYHOSTNAME=`/bin/hostname`
+
+BACKUP_PATH="/tmp"
+BACKUP_FILE="clish_${MYHOSTNAME}_${TIMESTAMP}.txt"
+
+#
+# Dump CLISH Configuration into file
+#
+/bin/clish -c "show configuration" > ${BACKUP_PATH}/${BACKUP_FILE}
+
+#
+# Transfer Dump to FTP Server
+#
+cd ${BACKUP_PATH}
+ftp -inv ${FTP_HOST} <<EOF
+user ${FTP_USER} ${FTP_PASS}
+cd ${MYHOSTNAME,,}
+mput ${BACKUP_FILE}
+bye
+EOF
